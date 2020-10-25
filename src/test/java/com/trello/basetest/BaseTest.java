5421177474d4d49397dbba72ff7fdc1cfac18005
Krzysztof Antczak <krantczak@pzu.pl>
2020-10-25 23:15:18 +0100

    [Feature] Read trello authorization keys from .properties file;

--- src/test/java/com/trello/basetest/BaseTest.java
+++ src/test/java/com/trello/basetest/BaseTest.java
@@ -4,11 +4,17 @@ import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
-    protected static final String KEY = "XXX";
-    protected static final String TOKEN = "XXX";
+    protected static String KEY = "XXX";
+    protected static String TOKEN = "XXX";
 
     protected static final String BASE_URL = "https://api.trello.com/1/";
     protected static final String BOARDS = "boards/";
@@ -22,6 +28,10 @@ public class BaseTest {
 
     @BeforeAll
     public static void beforeAll() {
+        Configuration config = loadConfig();
+        KEY = config.getString("trello.key");
+        TOKEN = config.getString("trello.token");
+
         reqBuilder = new RequestSpecBuilder();
         reqBuilder.addQueryParam("key", KEY);
         reqBuilder.addQueryParam("token", TOKEN);
@@ -30,5 +40,18 @@ public class BaseTest {
         reqSpec = reqBuilder.build();
 
         faker = new Faker();
+    }
+
+    private static FileBasedConfiguration loadConfig() {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.properties().setFileName("test.properties"));
+        try {
+            return builder.getConfiguration();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
